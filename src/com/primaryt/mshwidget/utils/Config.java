a13bf2d4634a94e92a05b01c5a53828bbc5ade4b
Kumar Bibek <coomar.101@gmail.com>
2012-06-08 22:39:54 +0530

    Changed the logging and debug flags

--- src/com/primaryt/mshwidget/utils/Config.java
+++ src/com/primaryt/mshwidget/utils/Config.java
@@ -2,7 +2,7 @@
 package com.primaryt.mshwidget.utils;
 
 public class Config {
-    public final static boolean LOGGING = true;
+    public final static boolean LOGGING = false;
 
-    public final static boolean DEBUG = true;
+    public final static boolean DEBUG = false;
 }
